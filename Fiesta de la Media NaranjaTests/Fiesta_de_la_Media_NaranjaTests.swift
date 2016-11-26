a106f52490256975b225e425d5ac074cae964be0
Pablo Aznar <pabloaznar93@gmail.com>
2016-11-26 18:38:37 +0100

    Initial Commit

---
+++ Fiesta de la Media NaranjaTests/Fiesta_de_la_Media_NaranjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fiesta_de_la_Media_NaranjaTests.swift
+//  Fiesta de la Media NaranjaTests
+//
+//  Created by Pablo on 26/11/16.
+//  Copyright © 2016 Pablo. All rights reserved.
+//
+
+import XCTest
+@testable import Fiesta_de_la_Media_Naranja
+
+class Fiesta_de_la_Media_NaranjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
